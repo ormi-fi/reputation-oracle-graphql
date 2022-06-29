55e02f4f8ce41651e70cbfe282e3b16c94c4955e
Le Yu <leyu00@gmail.com>
2022-06-29 19:06:41 -0400

    Usee index file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import { ApolloServer } from 'apollo-server'
 import { schema } from './schema'
-import { dataSources } from './dataSources/moralisDataSource'
+import { dataSources } from './dataSources'
 require('dotenv').config()
 
-
-
 const server = new ApolloServer({ schema, dataSources })
-
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`)
