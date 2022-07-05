5ef729fa265a41717162daac830534919551efdd
Victor Fei <vf63@cornell.edu>
2022-07-05 10:40:33 -0700

    updated tulons version (#2)
    The latest Tulons version uses Axios which can run in node environment, so updated package.json to point to latest Tulons.
    Also changed Passport | CeramicPassport types as gitcoin-sdk-reader made such refactoring

--- src/dataSources/gitcoinPassportDataSource.ts
+++ src/dataSources/gitcoinPassportDataSource.ts
@@ -1,6 +1,7 @@
-import { PassportReader, CeramicPassportRecord, CeramicPassportStream } from '@gitcoinco/passport-sdk-reader';
+import { PassportReader, CeramicPassport } from '@gitcoinco/passport-sdk-reader';
+import { Passport } from '@gitcoinco/passport-sdk-types';
 import { DataSource } from 'apollo-datasource';
-import 'dotenv/config'
+import 'dotenv/config';
 
 const ceramicNodeUrl = process.env.CERAMIC_NODE_URL;
 const ceramicNetwork = '1'; // Single network. Hardcoded for now.
@@ -13,7 +14,7 @@ class GitcoinPassportAPI extends DataSource {
     }
 
     // Gets the fully hydrated passport. Returns false if no data
-    getPassport = async (ethereumAddress: string): Promise<CeramicPassportStream | CeramicPassportRecord | false> => {
+    getPassport = async (ethereumAddress: string): Promise<CeramicPassport | Passport | false> => {
         return await this.reader.getPassport(ethereumAddress);
     }
 }
